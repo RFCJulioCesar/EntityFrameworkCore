e77736511a2f62e606bdd7e9e992fc37035cd2ce
RFCJulioCesar <juliosishui@gmail.com>
2024-09-24 10:24:15 -0600

    mi primer commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mi_primer_conexion_BD
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {
+
+        }
+    }
+}
+    
